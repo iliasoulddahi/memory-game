fccb5dde5f1d76adbd08773f25bb0bbdf6de6454
mmacnesa <mmacnesa@gmail.com>
2022-12-07 19:22:01 +0700

    update malam

--- app/game.js
+++ app/game.js
@@ -1,1 +1,76 @@
-// functions
+const boxGroub = [
+    {
+        src:"assets/flag_png/argentina.png",
+        terbuka:false
+    },
+    {
+        src:"assets/flag_png/croatia.png",
+        terbuka:false
+    },
+    {
+        src:"assets/flag_png/croatia.png",
+        terbuka:false
+    },
+    {
+        src:"assets/flag_png/argentina.png",
+        terbuka:false
+    },
+    {
+        src:"assets/flag_png/croatia.png",
+        terbuka:false
+    },
+    {
+        src:"assets/flag_png/argentina.png",
+        terbuka:false
+    },
+    {
+        src:"assets/flag_png/croatia.png",
+        terbuka:false
+    },
+    {
+        src:"assets/flag_png/argentina.png",
+        terbuka:false
+    }
+]
+
+// akses dom ke main
+const main = document.querySelector('#main')
+let chekerCollection = []
+
+// menampilkan & menambahkan semua event ke per box
+for (let i = 0; i < boxGroub.length; i++) {
+    const element = boxGroub[i];
+    
+    // pembuatan elemen baru untuk box
+    const box = document.createElement('div')
+    const img = document.createElement('img')
+    img.src = "assets/flag_png/icon tanda tanya.png"
+    box.id= "box"+i
+    box.classList.add('box')
+    box.appendChild(img)
+    // ngepush ke elemen main
+    main.appendChild(box)
+
+    //PROSES PEMBUATAN EVENT DI SETIAP
+    box.addEventListener('click', function(){
+        img.src = element.src
+        
+        if(chekerCollection.length <= 1) {
+            chekerCollection.push(element.src)
+            console.log(chekerCollection)
+            // melakukan pengecekan
+            if(chekerCollection[0] === chekerCollection[1]) {
+                element.terbuka = true
+                boxGroub[i-1].terbuka = true
+            } 
+        }else {
+
+            chekerCollection = []
+        }
+        //
+        console.log(element.terbuka , boxGroub[i-1].terbuka)
+        
+    })
+}
+
+// argentina === coratia -> terbuka = tertutup
--- app/ui.js
+++ app/ui.js
@@ -1,15 +1,17 @@
 // // const div = document.querySelector('.box')
 
+// document.querySelector('body').style.backgroundImage = 'url(assets/page/Background.png)'
+
 const elems = document.querySelectorAll('.box') ;
 
 elems.forEach(element =>  {
     let qimg = document.createElement('img') //quest img
     qimg.className = 'quest'
-    qimg.src = "assets/flag png/icon tanda tanya.png" ;  
+    qimg.src = "assets/flag_png/icon_tandaTanya.png" ;  
     
     let fimg = document.createElement('img') // flag img
     fimg.className = 'flag'
-    fimg.src = "assets/flag png/argentina.png" ;  
+    fimg.src = "assets/flag_png/argentina.png" ;  
     fimg.style.display = 'none'
 
     element.appendChild(qimg) ; element.appendChild(fimg) 

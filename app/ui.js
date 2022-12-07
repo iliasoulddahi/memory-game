bd510e56866eb3e8c114bd6d280a0a23aef4b597
mmacnesa <mmacnesa@gmail.com>
2022-12-07 18:34:39 +0700

    updataMagrib

--- app/ui.js
+++ app/ui.js
@@ -1,5 +1,41 @@
-const div = document.querySelector('div')
+// // const div = document.querySelector('.box')
 
-div.addEventListener('click', function(){
-    this.style = 'color:red;'
+const elems = document.querySelectorAll('.box') ;
+
+elems.forEach(element =>  {
+    let qimg = document.createElement('img') //quest img
+    qimg.className = 'quest'
+    qimg.src = "assets/flag png/icon tanda tanya.png" ;  
+    
+    let fimg = document.createElement('img') // flag img
+    fimg.className = 'flag'
+    fimg.src = "assets/flag png/argentina.png" ;  
+    fimg.style.display = 'none'
+
+    element.appendChild(qimg) ; element.appendChild(fimg) 
 })
+
+elems.forEach(element => element.addEventListener('click', e => { 
+    if(!element.classList.contains('rotate')){
+        element.classList.add('rotate')
+        function show(){
+           element.querySelector('.flag').style.display = 'block';
+        }
+        function hide(){
+           element.querySelector('.quest').style.display = 'none';
+        } 
+    } 
+    else {
+        element.classList.remove('rotate')
+        function hide(){
+            element.querySelector('.flag').style.display = 'none';
+        }
+        function show(){
+            element.querySelector('.quest').style.display = 'block';
+        }
+        
+    } 
+    setTimeout(show, 300);
+    setTimeout(hide, 290);  
+})) 
+

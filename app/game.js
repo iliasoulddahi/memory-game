279610b67cbf145464e91eb3098cba78aedc1afe
iliasoulddahi <iliasoulddahi@gmail.com>
2022-12-08 15:39:44 +0700

    game logic done part1

--- app/game.js
+++ app/game.js
@@ -1,76 +1,141 @@
-const boxGroub = [
-    {
-        src:"assets/flag_png/argentina.png",
-        terbuka:false
-    },
-    {
-        src:"assets/flag_png/croatia.png",
-        terbuka:false
-    },
-    {
-        src:"assets/flag_png/croatia.png",
-        terbuka:false
-    },
-    {
-        src:"assets/flag_png/argentina.png",
-        terbuka:false
-    },
-    {
-        src:"assets/flag_png/croatia.png",
-        terbuka:false
-    },
-    {
-        src:"assets/flag_png/argentina.png",
-        terbuka:false
-    },
-    {
-        src:"assets/flag_png/croatia.png",
-        terbuka:false
-    },
-    {
-        src:"assets/flag_png/argentina.png",
-        terbuka:false
+// // const div = document.querySelector('.box')
+
+// document.querySelector('body').style.backgroundImage = 'url(assets/page/Background.png)'
+const flags = [
+    "assets/flag_png/argentina.png",
+    "assets/flag_png/australia.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+    "assets/flag_png/brazil.png",
+  ];
+  
+  function shuffle(array) {
+    let i = array.length,
+        j = 0,
+        temp;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        // swap randomly chosen element with current element
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
-]
-
-// akses dom ke main
-const main = document.querySelector('#main')
-let chekerCollection = []
-
-// menampilkan & menambahkan semua event ke per box
-for (let i = 0; i < boxGroub.length; i++) {
-    const element = boxGroub[i];
-    
-    // pembuatan elemen baru untuk box
-    const box = document.createElement('div')
-    const img = document.createElement('img')
-    img.src = "assets/flag_png/icon tanda tanya.png"
-    box.id= "box"+i
-    box.classList.add('box')
-    box.appendChild(img)
-    // ngepush ke elemen main
-    main.appendChild(box)
-
-    //PROSES PEMBUATAN EVENT DI SETIAP
-    box.addEventListener('click', function(){
-        img.src = element.src
-        
-        if(chekerCollection.length <= 1) {
-            chekerCollection.push(element.src)
-            console.log(chekerCollection)
-            // melakukan pengecekan
-            if(chekerCollection[0] === chekerCollection[1]) {
-                element.terbuka = true
-                boxGroub[i-1].terbuka = true
-            } 
-        }else {
-
-            chekerCollection = []
+    return array;
+  }
+  
+  let randomFlags = shuffle(flags);
+  
+  
+  
+  
+  const boxes = document.querySelectorAll(".box");
+  
+  for (let i = 0; i < boxes.length; i++) {
+    const element = boxes[i];
+    let qimg = document.createElement("img"); //quest img
+    qimg.className = "quest";
+    qimg.src = "assets/flag_png/icon_tandaTanya.png";
+  
+    let fimg = document.createElement("img"); // flag img
+    fimg.className = "flag";
+    fimg.src = randomFlags[i];
+    fimg.style.display = "none";
+  
+    element.appendChild(qimg);
+    element.appendChild(fimg);
+  }
+  
+  let terbuka = [];
+  for (let i = 0; i < boxes.length; i++) {
+    const element = boxes[i];
+    element.id = 'box'+i
+  
+    element.addEventListener("click", (e) => {
+      terbuka.push(element.id);
+      
+  
+      //variable
+      let boxSekarang = document.querySelector('#'+terbuka[1])
+      let boxTerakhir = document.querySelector('#'+terbuka[0])
+      // console.log(boxSekarang.lastChild.src, 'box sekarang')
+      // console.log(boxTerakhir.lastChild.src, 'box terakhir')
+      console.log(terbuka, 'terbuka')
+      function showQuest(el) {
+        el.classList.remove("rotate");
+        function hide() {
+          el.querySelector(".flag").style.display = "none";
         }
-        //
-        console.log(element.terbuka , boxGroub[i-1].terbuka)
-        
-    })
-}
-
-// argentina === coratia -> terbuka = tertutup
+        function show() {
+          el.querySelector(".quest").style.display = "block";
+        }
+  
+        setTimeout(show, 300);
+        setTimeout(hide, 290);
+      }
+  
+      function showFlag(el) {
+        el.classList.add("rotate");
+        function show() {
+          el.querySelector(".flag").style.display = "block";
+        }
+        function hide() {
+          el.querySelector(".quest").style.display = "none";
+        }
+        setTimeout(show, 300);
+        setTimeout(hide, 290);
+      }
+      
+      showFlag(element)
+  
+      if (terbuka.length > 1) {
+        if(boxSekarang.lastChild.src !== boxTerakhir.lastChild.src){
+          setTimeout(function(){
+            //code goes here
+            showQuest(boxSekarang)
+            showQuest(boxTerakhir)
+         }, 1000);
+          
+        }
+        terbuka = []
+      }  // return apabila yang terbuka sudah 2
+      
+    });
+  }
+  
+  
+  
+  
+  //TIMER
+  var d = new Date();
+  var millisecondssince1970 = d.getTime();
+  var newMillisec = millisecondssince1970 + (400 * 60);
+  
+  var countDownDate = new Date(newMillisec);
+  
+  var x = setInterval(function() {
+  
+    var now = new Date().getTime();
+  
+    var distance = countDownDate - now;
+  
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+  
+    if (distance < 0) {
+      //DIEKSEKUSI SETELAH WAKTU HABIS
+      main.innerHTML = "<h1>WAKTU HABIS</h1>"
+    }
+  }, 1000);
